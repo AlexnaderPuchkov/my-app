76bd5d70335378621e69090a4c19a32820c5fd35
AlexanderPuchkov <zaneiyaaa@gmail.com>
2022-01-11 17:01:30 +0500

    .

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './styles/App.css';
+import Post from './components/Post';
 
 function App() {
+    
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Post post={{id: 1, title: 'Maxim', body: 'Petuch'}} />
+      <Post post={{id: 2, title: 'Sasha', body: 'Petuch'}} />
+      <Post post={{id: 3, title: 'Maxim', body: 'Petuch'}} />
+      <Post post={{id: 4, title: 'Sasha', body: 'Petuch'}} />
+      <Post post={{id: 5, title: 'Maxim', body: 'Petuch'}} />
     </div>
   );
 }
